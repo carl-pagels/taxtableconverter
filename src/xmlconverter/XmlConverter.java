abcb3de907b963609d9a05cf46d39beb089c08b1
carl.pagels <carl.pagels@fortnox.se>
2020-09-23 19:55:09 +0200

    Added error handling to xml converter

--- src/xmlconverter/XmlConverter.java
+++ src/xmlconverter/XmlConverter.java
@@ -17,45 +17,70 @@ import java.io.*;
 public class XmlConverter {
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException {
-        Document doc;
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File("/Users/carl.pagels/repos/taxtableconverter/src/xmlconverter/cities.csv")))) {
-
-            String firstLine = reader.readLine();
-            //Assumes that the first line is the columns. Filter out illegal characters for xml files
-            final String[] categories = firstLine.replace("\"", "").replace(" ", "").split(",");
-
-            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            docFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-            docFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
-            doc = docFactory.newDocumentBuilder().newDocument();
-
-            Element root = doc.createElement("Xmlconverter");
-            doc.appendChild(root);
+        Document document;
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("/Users/carl.pagels/repos/taxtableconverter/src/xmlconverter/hej.csv")))) {
+            final String[] categories = getCategories(reader.readLine());
+            document = createDocument();
+            Element root = document.createElement("Xmlconverter");
+            document.appendChild(root);
 
             for (String line; (line = reader.readLine()) != null; ) {
+                // One tag named row for each line in csv file
+                Element row = document.createElement("row");
                 final String[] values = line.split(",");
-                Element row = doc.createElement("row");
                 for (int i = 0; i < categories.length; i++) {
-                    if (values.length <= i) {
-                        break;
+                    final String category = categories[i];
+                    if (category.isEmpty()) {
+                        continue;
                     }
-                    final Element categoryElement = createCategoryElement(doc, categories[i], values[i]);
+                    String value = "";
+                    if (values.length > i) {
+                        value = values[i];
+                    }
+                    final Element categoryElement = createCategoryElement(document, category, value);
                     row.appendChild(categoryElement);
                     root.appendChild(row);
                 }
             }
         }
+        generateXMlFile(document);
+    }
+
+    private static void generateXMlFile(Document document) throws TransformerException, IOException {
         TransformerFactory tf = TransformerFactory.newInstance();
         tf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
         Transformer transformer = tf.newTransformer();
         transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
         StringWriter writer = new StringWriter();
-        transformer.transform(new DOMSource(doc), new StreamResult(writer));
+        transformer.transform(new DOMSource(document), new StreamResult(writer));
 
         File file = new File("generated.xml");
         try (FileWriter fileWriter = new FileWriter(file)) {
             fileWriter.write(writer.getBuffer().toString());
         }
+    }
+
+    private static Document createDocument() throws ParserConfigurationException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        docFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        docFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+        return docFactory.newDocumentBuilder().newDocument();
+    }
+
+    /**
+     * // Assumes that the first line is the columns. Filter out illegal characters for xml files
+     * @param line The initial line to get categories from
+     * @return with illegal characters escaped
+     */
+    private static String[] getCategories(String line) {
+        return line
+                .replace("\"", "")
+                .replace(" ", "")
+                .replace("#", "")
+                .replace("(", "_x0028_")
+                .replace(")", "_x0029_")
+                .replace("?", "")
+                .split(",");
     }
 
     private static Element createCategoryElement(Document doc, String category, String value) {
